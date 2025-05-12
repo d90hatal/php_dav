8eea909b159cea0d8a97c132958c2c82d1f365c2
david <d90hatal@gmail.com>
2025-05-12 16:58:12 +0300

    flexible confige

--- index.php
+++ index.php
@@ -6,21 +6,16 @@ require "functions.php";
 
 //connect to mysql database.
 
-$dsn = "mysql:host=localhost;port=3306;dbname=php_db;user=root;charset=utf8mb4";
+require "Database.php";
 
-$pdo = new PDO($dsn);
+$config = require "config.php";
 
-$statement = $pdo->prepare("SELECT title FROM untitled_table_1");
 
-$statement->execute();
+$db = new Database($config['database']);
 
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+$posts = $db->query("SELECT * FROM untitled_table_1")->fetchAll();
 
+// foreach ($posts as $post) {
+//     echo "<li>" . $post['title'] . "</li>";
+// }
 dd($posts);
-
-
-
-
-
-
-
