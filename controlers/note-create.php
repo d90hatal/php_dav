4d9427feda88f3fe8c339ed14d54fe8921bf0222
david <d90hatal@gmail.com>
2025-07-02 15:25:16 +0300

    Extract a Simple Validator Class

--- controlers/note-create.php
+++ controlers/note-create.php
@@ -3,31 +3,35 @@
 
 require 'Validator.php';
 
-$config = require('config.php');
+$config = require 'config.php';
 $db = new Database($config['database']);
 
 $heading = "Create Note";
 
-$errors = ['body' => 'empty'];
-
+// $errors = ['body' => ''];
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $errors = [];
 
-    $validator = new Validator();
-    if (! $validator::string($_POST['body'] , 1, 15)) {
+
+
+    // $validator = new Validator();
+
+    if (! Validator::string($_POST['body'], 1, 15)) {
         $errors['body'] = 'Note must be contain a character and less than 15 characters';
     }
-    
-    }
-    if ($errors['body'] === 'empty') {
+    if (empty($errors)) {
         $db->query('INSERT INTO `notes` (`body`, `user_id`) VALUES(:body, :user_id)', [
             'body' => $_POST['body'],
-            'user_id' => 1,
+            'user_id' => 1
         ]);
         header('Location: /php-sandbox/php_dav/notes');
         exit();
     }
+   
+}
+
 
 
 require "views/note-create.view.php";
