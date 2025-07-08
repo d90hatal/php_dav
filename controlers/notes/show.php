1710e8828043ce8b06083233befcc361be41ed7d
david <d90hatal@gmail.com>
2025-07-08 16:32:10 +0300

    delete note

--- controlers/notes/show.php
+++ controlers/notes/show.php
@@ -6,8 +6,24 @@ $config = require base_path("config.php");
 
 $db = new Database($config['database']);
 
-
 $currentUserId = 1;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+    $note = $db->query("SELECT * FROM notes WHERE id = :id", [
+        "id" => $_GET['id']
+    ])->findOrFail();
+    
+    authorize($note['user_id'] === $currentUserId);
+
+    $db->query("DELETE FROM notes WHERE id = :id", [
+        "id" => $_GET['id']
+    ]);
+    header('location: /php-sandbox/php_dav/notes');
+    exit();
+}
+else{
+
 
 $id = $_GET['id'];
 
@@ -23,3 +39,5 @@ view("notes/show.view.php", [
     "heading" => "note",
     "note" => $note
 ]);
+
+}
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -5,6 +5,11 @@
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                 <p><?= $note['body'] ?></p>
                 <a href="/php-sandbox/php_dav/notes" class="text-blue-500 hover:underline">go back</a>
+                <br><br>
+                <form  method="POST">
+                    <input type="hidden" name="id" value="<?= $note['id'] ?>">
+                    <button class="text-red-500 hover:underline">Delete</button>
+                </form>
             </div>
         </main>
 <?php require base_path('views/partials/footer.php') ?>
